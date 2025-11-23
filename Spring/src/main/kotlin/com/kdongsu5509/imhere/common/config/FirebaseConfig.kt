ab7ae6c1315197022a46c8cf49fd3a2a677166de
kdongsu5509 <kod66170@gmail.com>
2025-11-23 21:53:00 +0900

    refactor(issue40) : move file
    파일 위치를 변경이 필요한 것으로 판단. 해당 파일을 쓰는 유일한 위치로 이동

--- Spring/src/main/kotlin/com/kdongsu5509/imhere/common/config/FirebaseConfig.kt
+++ Spring/src/main/kotlin/com/kdongsu5509/imhere/common/config/FirebaseConfig.kt
@@ -3,7 +3,6 @@ package com.kdongsu5509.imhere.common.config
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import com.kdongsu5509.imhere.notification.adapter.out.FcmProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
