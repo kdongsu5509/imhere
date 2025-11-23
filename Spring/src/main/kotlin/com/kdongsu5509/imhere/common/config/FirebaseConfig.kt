f3e049560993e6cc5cd6b98e5642d6e108045b40
kdongsu5509 <kod66170@gmail.com>
2025-11-23 21:09:01 +0900

    feat(issue) : add code to initialize fcm on server

---
+++ Spring/src/main/kotlin/com/kdongsu5509/imhere/common/config/FirebaseConfig.kt
@@ -0,0 +1,29 @@
+package com.kdongsu5509.imhere.common.config
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.kdongsu5509.imhere.notification.adapter.out.FcmProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import java.io.InputStream
+
+@Configuration
+@EnableConfigurationProperties(FcmProperties::class)
+class FirebaseConfig(
+    private val fcmProperties: FcmProperties
+) {
+    @Bean
+    fun firebaseApp(): FirebaseApp {
+        val resource = ClassPathResource(fcmProperties.path)
+        val serviceAccount: InputStream = resource.inputStream
+
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build()
+
+        return FirebaseApp.initializeApp(options)
+    }
+}
